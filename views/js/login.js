bd86ae6ddc400d5be20435e5d9ff7fccc178bb9d
Honey-Bi <biten10@naver.com>
2023-06-08 14:24:22 +0900

    로그인 구현

---
+++ views/js/login.js
@@ -0,0 +1,48 @@
+$('#loginBtn').click(function(){
+    $('#typeEmailX').val();
+    $('#typePasswordX').val();
+});
+
+
+$('#loginForm').submit(function(){
+    let inputId = $('#typeEmailX').val(), 
+        inputPw = $('#typePasswordX').val();
+
+    if (!inputId) {
+        $('#typeEmailX').focus();
+        $('.err-text').text("아이디를 입력해주세요");
+        $('.err-text').css('color', '#ff3f3f');
+        return;
+    }
+    if (!inputPw) {
+        $('#typePasswordX').focus();
+        $('.err-text').text("비밀번호를를 입력해주세요");
+        $('.err-text').css('color', '#ff3f3f');
+        return;
+    }
+
+    $.ajax({
+        method:'POST',                                           
+        url:'/account/login-confirm',
+        async: true,
+        data: {
+            id: inputId,
+            pw: inputPw
+        },
+        dataType: 'json',
+        success: function(result){
+            console.log("login");
+            // $.cookie('token', result.token);
+            document.cookie = "token="+result.token;
+            // location.replace('/main');
+            return;
+        },
+        error: function(error) {
+            console.log(error)
+            var err = $('.err-text').text("아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.");
+            err.html(err.html().replace(/\n/g, '<br/>'));
+            $('.err-text').css('color', '#ff3f3f');
+            return;
+        }
+    });
+});
