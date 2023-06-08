0b345e51ce543ed31d7952cbbd0891d85f762273
Honey-Bi <biten10@naver.com>
2023-06-08 14:22:12 +0900

    user 스키마 생성

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose"); // mongoose 불러오기
+
+// Schema 생성
+const UserSchema = new mongoose.Schema({
+  nick: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// model을 export 해주기
+module.exports = User = mongoose.model("user", UserSchema);
