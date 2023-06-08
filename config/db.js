4b3ab687535584dfd0d2112381196c0d0d339962
Honey-Bi <biten10@naver.com>
2023-06-08 14:21:35 +0900

    db 연결

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require('./default.json');
+// URI
+const uri = config.mongoURI;
+
+// Connect MongoDB  
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
