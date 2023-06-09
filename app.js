890457228c63c2fc575f09dd71ff6bd3af60a62d
Honey-Bi <biten10@naver.com>
2023-06-09 15:01:42 +0900

    404페이지 연결

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require ('path');
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
 
+const userStatus = require('./middleware/userStatus');
 const connectDB = require("./config/db");
 connectDB();
 
@@ -16,20 +18,30 @@ app.use('/js', express.static(path.join(__dirname, '/views/js')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(expressSession({
+  secret: 'my key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 const account = require('./routes/account');
 const main = require('./routes/main');
+// const { auth } = require('./middleware/userValidation');
 
 app.use('/account', account);
 app.use('/main', main);
 
-app.get('/', (req, res) => {
-  res.render('', {title: 'ORACULUM'});
+app.get('/', async (req, res) => {
+  res.render('',{ title: 'Oraculum', isLogin: userStatus.isLogin(req) });
 });
+
+app.all('*', (req,res) => {
+  // res.render('./404', {title: '404 Not', isLogin: userStatus.isLogin(req) });
+  res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>')
+})
 
 
 app.listen(8080, (err) => {
   if (err) return console.log(err);
   console.log("The server is listening on port 8080");
 });
-
