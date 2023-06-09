d7fd423b4926dc35c9841e7561c39bb6282395c9
Honey-Bi <biten10@naver.com>
2023-06-09 15:00:41 +0900

    메인 라우터설정 및 안쓰는 코드 삭제

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 const { auth } = require('../middleware/userValidation');
+const userStatus = require('../middleware/userStatus')
 const jwt = require('jsonwebtoken');
 const config = require('../config/default.json');
 const SECRET_KEY = config.jwtSecretKey;
 
-router.get('/', (req, res) => {
-    res.render('./main');
-});
-
-// router.get('/main', (req, res) => {
-//     res.render('./main/');
+// router.get('/', auth, async (req, res) => {
+//     const id = req.decoded.user.id;
+//     user = await User.findOne({ _id: id }) ;
+//     res.render('./main', {title: 'main', userName: user.name, isLogin: true});
 // });
+
+
+router.get('/', (req, res) => {
+    res.render('./main', {title: 'main', isLogin: true,});
+});
 
 module.exports = router;
