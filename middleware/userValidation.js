eb567b93bbc24641b8fd45d0f6d927d81eba37ae
Honey-Bi <biten10@naver.com>
2023-06-09 15:01:16 +0900

    유저 토큰 검사 후 없을시 리다이렉션 추가

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/default.json');
+const SECRET_KEY = config.jwtSecretKey;
 
 exports.auth = (req, res, next) => {
     // 인증 완료
     try {
         // 요청 쿠키에 저장된 토큰과 비밀키를 사용하여 토큰을 req.decoded에 반환
-        req.decoded = jwt.verify(req.cookies.token, config.jwtSecretKey);
+        req.decoded = jwt.verify(req.session.token, SECRET_KEY);
         return next();
     }
     // 인증 실패
@@ -19,6 +20,7 @@ exports.auth = (req, res, next) => {
         }
         // 토큰의 비밀키가 일치하지 않는 경우
         if (error.name === 'JsonWebTokenError') {
+            return res.redirect('/account/login');
             return res.status(401).json({
                 code: 401,
                 message: '유효하지 않은 토큰입니다.'
