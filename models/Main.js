928ba28599b1f9a042d3003addf78f53ab9b8dc2
Honey-Bi <biten10@naver.com>
2023-06-12 14:06:56 +0900

    데이터 스키마 설계

--- models/Main.js
+++ models/Main.js
@@ -3,16 +3,55 @@ const mongoose = require("mongoose"); // mongoose 불러오기
 // Schema 생성
 const MainSchema = new mongoose.Schema({
     userId: new mongoose.Types.ObjectId,
-    nowEvent: {
+    nowEvent: new mongoose.Types.ObjectId,
+    fuel: {
         type: Number,
-        default: 0
+        default: 50,
+        min: 0,
+        max: 100
+    },  
+    resourse: {
+        type: Number,
+        default: 50,
+        min: 0,
+        max: 100
     },
-    
+    technology: {
+        type: Number,
+        default: 50,
+        min: 0,
+        max: 100
+    },
+    risk: {
+        type: Number,
+        default: 50,
+        min: 0,
+        max: 100
+    },
     created : {
         type : Date,
         default : Date.now
     } 
-  });
-  
-  // model을 export 해주기
-  module.exports = Main = mongoose.model("main", MainSchema);
+
+});
+
+const EventSchema = new mongoose.Schema({
+    contents: {
+        type: String,
+        required: true
+    },
+    r_text: {
+        type: String,
+        required: true,
+    },
+    l_text: {
+        type: String,
+        required: true,
+    },
+    r_result: [Number],
+    l_result: [Number]
+});
+
+// model을 export 해주기
+module.exports = Main = mongoose.model("main", MainSchema);
+module.exports = mongoose.model("event", EventSchema);
