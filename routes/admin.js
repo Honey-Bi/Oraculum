d5de78493ba882f5dceb60f216e80a98e52f0bb7
Honey-Bi <biten10@naver.com>
2023-06-13 14:42:22 +0900

    관리자 라우터 생성

---
+++ routes/admin.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { auth } = require('../middleware/userValidation');
+const User = require('../models/User'); 
+
+router.get('/', auth, async (req, res) => {
+    const id = req.decoded.user.id;
+    try {
+        let user = await User.findOne({ _id: id }) ;
+        if (user.access == 1) {
+            res.render('./admin/', {});
+        } else {
+            return res.redirect('/404');
+        }
+    } catch (error) {
+        
+    }
+});
+
+router.get('/management', auth, async (req, res) => {
+    const id = req.decoded.user.id;
+    try {
+        let user = await User.findOne({ _id: id }) ;
+        if (user.access == 1) {
+            let data = await User.find({}, {_id: 0, password: 0, __v: 0,});
+            // console.log(data[0])
+
+            keys = await User.findOne({});
+            for(n in keys) {
+                console.log(n);
+            }
+            res.render('./admin/management', {data: data});
+        } else {
+            res.redirect('/404');
+        }
+    } catch (error) {   
+        console.log(error)
+    }
+});
+
+module.exports = router;
