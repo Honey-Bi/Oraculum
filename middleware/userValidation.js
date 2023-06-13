f05bcc7a4a3122c0365167e3e5fef18890a597f4
Honey-Bi <biten10@naver.com>
2023-06-13 14:43:25 +0900

    토큰 만료시 코드 변경 (추가중)

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -13,18 +13,19 @@ exports.auth = (req, res, next) => {
     catch (error) {
         // 유효시간이 초과된 경우
         if (error.name === 'TokenExpiredError') {
-            return res.status(419).json({
-                code: 419,
-                message: '토큰이 만료되었습니다.'
-            });
+            return res.write("<script>alert('code: 419, message: 토큰이 만료되었습니다. 다시 로그인해 주세요'); location.href='/account/logout'</script>");
+            // return res.redirect('/account/logout');
+            // return res.status(419).json({
+                
+            // });
         }
         // 토큰의 비밀키가 일치하지 않는 경우
         if (error.name === 'JsonWebTokenError') {
-            res.status(401).json({
-                code: 401,
-                message: '유효하지 않은 토큰입니다.'
-            });
             return res.redirect('/account/login');
+            // return res.status(401).json({
+            //     code: 401,
+            //     message: '유효하지 않은 토큰입니다.'
+            // });
         }
     }
 }
