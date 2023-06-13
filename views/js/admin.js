a2565fae91ccce11ebd7e2c33b5c33f37faa758b
Honey-Bi <biten10@naver.com>
2023-06-13 14:41:44 +0900

    관리자용 페이지 생성

---
+++ views/js/admin.js
@@ -0,0 +1,16 @@
+var url_href = window.location.href;
+
+var url = new URL(url_href);
+var management = url.searchParams.get("type");
+
+switch (management) {
+    case 'users':
+        $('.nav-item:nth-child(2) > .nav-link').addClass('active');
+        break;
+    case 'events':
+        $('.nav-item:nth-child(3) > .nav-link').addClass('active');
+        break;
+    default:
+        $('.nav-item:nth-child(1) > .nav-link').addClass('active');
+        break;
+}
