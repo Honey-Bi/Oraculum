6b48ef2a1a86b6b9a9bf2866c51f322ceb784fc3
Honey-Bi <biten10@naver.com>
2023-06-15 12:49:42 +0900

    관리자용 계정생성기능 추가

--- views/js/admin.js
+++ views/js/admin.js
@@ -1,20 +1,24 @@
-var url_href = window.location.href;
-
-var url = new URL(url_href);
-var management = url.searchParams.get("type");
-
-switch (management) {
-    case 'users':
-        $('.nav-item:nth-child(2) > .nav-link').addClass('active');
-        break;
-    case 'events':
-        $('.nav-item:nth-child(3) > .nav-link').addClass('active');
-        break;
-    default:
-        $('.nav-item:nth-child(1) > .nav-link').addClass('active');
-        break;
-}
-
 $('#addForm').submit(function(){
+    let inputName = $('#add_name').val(),
+        inputId = $('#add_email').val(),
+        password = $('#add_password').val();
+
     console.log('submit')
+    $.ajax({
+        method:"POST",                                           
+        url:"/account/register-confirm",
+        data: {
+            name: inputName,
+            id: inputId,
+            password: password,
+        },
+        dataType: "json",
+        success: function(result){
+            alert("정상적으로 추가되었습니다.");
+            location.reload();
+        },
+        error: function(result, status, error) {
+            console.log(error)
+        }
+    })
 });
