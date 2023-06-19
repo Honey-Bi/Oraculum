a82c202ed49427fc80906c53b21d02e7798394d0
Honey-Bi <biten10@naver.com>
2023-06-19 13:00:59 +0900

    메인 스탯 불러오기 기능 추가

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const { auth } = require('../middleware/userValidation');
 const userStatus = require('../middleware/userStatus')
 const jwt = require('jsonwebtoken');
 const config = require('../config/default.json');
+const Main = require('../models/Main');
 const SECRET_KEY = config.jwtSecretKey;
 
 // router.get('/', auth, async (req, res) => {
@@ -13,11 +14,40 @@ const SECRET_KEY = config.jwtSecretKey;
 
 
 router.get('/', auth, (req, res) => {
-
     res.render('./main', {title: 'main', isLogin: true,});
 });
 
 router.post('/select', auth, async (req, res) =>{
+    // console.log(main);
+});
+
+router.post('/getView', auth, async (req, res) => {
+    try {
+        let main = await Main.Main.findOne({
+            userId: req.decoded.user.id
+        }, {
+            fuel: 1,
+            resourse: 1,
+            technology: 1,
+            risk: 1
+        }).populate('nowEvent', {
+            contents: 1,
+            r_text: 1,
+            l_text: 1
+        });
+        let result = {
+            fuel: main.fuel,
+            resourse: main.resourse,
+            technology: main.technology,
+            risk: main.risk,
+            content: main.nowEvent.contents,
+            r_text: main.nowEvent.r_text,
+            l_text: main.nowEvent.l_text
+        }       
+        return res.status(200).send(result);
+    } catch (error) {
+        return res.status(400).send(error);    
+    }
     
 });
 
--- views/js/main.js
+++ views/js/main.js
@@ -1,12 +1,64 @@
 var minWidth = $(window).width() * 4 / 100; // 화면 크기의 4%
 var maxWidth = $(window).width() * 12 / 100; // 화면 크기의 12%
 
-$('.a').draggable({
+var selectItem = $('.selectBox');
+selectItem.css('height', selectItem.css('width'));
+$(window).resize(function() {
+    selectItem.css('height', selectItem.css('width'));
+});
+
+
+let fuel = $('#fuel'),
+    resource = $('#resource'),
+    technology = $('#technology'),
+    risk = $('#risk');
+
+$(document).ready(function () {
+    setView();
+});
+
+function setView() {
+    $.ajax({
+        method:'POST',                                           
+        url:'/main/getView',
+        async: true,
+        dataType: 'json',
+        success: function(result){
+            setContentView(result.content);
+            setStatsView(
+                result.fuel, 
+                result.resource, 
+                result.technology, 
+                result.risk
+            );
+            setSelectView(result.l_text, result.r_text);
+        },
+        error: function(request, status, error) {
+            console.log(error)
+            return;
+        }
+    });
+}
+function setContentView(text) {
+    $('.contents').text(text);
+}
+function setStatsView(fuel, resource, technology, risk) {
+    $('#fuelFill').css('height', 100-fuel+'%');
+    $('#resourceFill').css('height', 100-resource+'%');
+    $('#technologyFill').css('height', 100-technology+'%');
+    $('#riskFill').css('height', 100-risk+'%');
+}
+function setSelectView(left, right) {
+    $('#selectLeft').text(left);
+    $('#selectRight').text(right);
+}
+
+$('.selectBox').draggable({
     axis: "x", //좌우로만
     scroll: false,
     // containment : '', //화면 밖으로 이동 가능
     drag : function(){
-        var pos = $('.a').position(); // 드래그 하는 이미지의 위치값 알아내기
+        var pos = $('.selectBox').position(); // 드래그 하는 이미지의 위치값 알아내기
 
         $('.answer-left').css('opacity', (pos.left-minWidth)/maxWidth); 
         $('.answer-right').css('opacity', ((pos.left+minWidth)/maxWidth)*-1); 
@@ -30,6 +82,7 @@ $('.a').draggable({
             dataType: 'json',
             success: function(result){
                 console.log(result);
+                setView();
                 return;
             },
             error: function(error) {
@@ -37,7 +90,6 @@ $('.a').draggable({
                 return;
             }
         });
-        $('.title').text((select)?'right':'left');
         $(this).animate({ top : 0, left : 0}, 250, 'easeOutBack' ); // 원위치로, 제이쿼리UI 의 이징효과 사용
     }
 })
