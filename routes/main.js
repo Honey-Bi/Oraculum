d33ce2ffa842afd397356b21f83b3b6ff13ec42d
Honey-Bi <biten10@naver.com>
2023-06-20 15:18:30 +0900

    선택 기능 구현 및 게임 오버기능 추가

--- routes/main.js
+++ routes/main.js
@@ -18,7 +18,54 @@ router.get('/', auth, (req, res) => {
 });
 
 router.post('/select', auth, async (req, res) =>{
-    // console.log(main);
+    try {
+        let main = await Main.Main.findOne({
+            userId: req.decoded.user.id
+        }).populate('nowEvent');
+
+        var selectResult = [];
+        if (req.body.answer == 'right') {
+            selectResult = main.nowEvent.r_result;
+        } else if (req.body.answer == 'left') {
+            selectResult = main.nowEvent.l_result;
+        }
+        var stats = [ //현재 상태 저장
+            main.fuel,
+            main.resourse,
+            main.technology,
+            main.risk
+        ]
+
+        for (i in selectResult) { //선택 결과 계산
+            stats[i] += selectResult[i];
+        } 
+
+        let nextEventId = isOver(stats);
+        let nextEvent = main.nowEvent.next_event;
+
+
+        if (nextEventId) { // 게임 오버시
+            nextEvent = await Main.MainEvent.findOne({event_code: 4000 + nextEventId});
+        } else if (nextEvent === null){ //정해진 다음 이벤트가 없을경우
+            nextEvent = await Main.MainEvent.findOne({event_code: getRandom(1, 4)});
+        }
+
+        await Main.Main.updateOne({
+            _id: main._id
+        }, {
+            $set: {
+                nowEvent: nextEvent._id,
+                fuel: stats[0],
+                resourse: stats[1],
+                technology: stats[2],
+                risk: stats[3]
+            }
+        });
+        return res.status(200).send(true);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error);  
+    }
 });
 
 router.post('/getView', auth, async (req, res) => {
@@ -50,5 +97,40 @@ router.post('/getView', auth, async (req, res) => {
     }
     
 });
+
+function isOver(stats) {
+    for (i in stats) {
+        if(stats[i] >= 100) {
+            ++i;
+            return i;
+        } else if(stats[i] <= 0) {
+            ++i;
+            return i*2;
+        }
+    }
+    return 0;
+    // 1 = 연료과다
+    // 2 = 자원과다
+    // 3 = 기술과다
+    // 4 = 위험과다
+    // 5 = 연료부족
+    // 6 = 자원부족
+    // 7 = 기술부족
+    // 8 = 위험부족
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max+1 - min) + min);
+}   
+
+function arrayMax(arr) {
+    var len = arr.length, max = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return arr.indexOf(max);
+};
 
 module.exports = router;
