7f350083ab0a8eeb381af183550ff9f22764c518
Honey-Bi <biten10@naver.com>
2023-06-21 14:53:31 +0900

    형태변환에 따른 유지보수

--- views/js/admin.js
+++ views/js/admin.js
@@ -27,15 +27,21 @@ let r_result=[], l_result=[];
 
 $('input[name=results]').change(function() {    
     if ($('input[name=results]:checked').val() == 'left') {
-        $('input[name=add_result]').each((index, item) => {
-            r_result[index] = $(item).val()*1;
-            $(item).val(l_result[index]);
-        });
+        $('.left-stats').css('display', 'flex');
+        $('.right-stats').css('display', 'none');
     } else if ($('input[name=results]:checked').val() == 'right'){
-        $('input[name=add_result]').each((index, item) => {
-            l_result[index] = $(item).val()*1;
-            $(item).val(r_result[index]);
-        });
+        $('.left-stats').css('display', 'none');
+        $('.right-stats').css('display', 'flex');
+    }
+});
+
+$('input[name=next_event]').change(function() {    
+    if ($('input[name=next_event]:checked').val() == 'left') {
+        $('.left-next').css('display', 'flex');
+        $('.right-next').css('display', 'none');
+    } else if ($('input[name=next_event]:checked').val() == 'right'){
+        $('.left-next').css('display', 'none');
+        $('.right-next').css('display', 'flex');
     }
 });
 
@@ -50,35 +56,36 @@ $('.addEvent').click(function() {
     $('#editBtn').css('display', 'none');
     $('#saveBtn').css('display', 'block');
 });
-$('#eventForm').submit(function() {
-    if ($('#left_result').is(':checked')) {
-        $('input[name=add_result]').each((index, item) => {
-            l_result[index] = $(item).val()*1;
-        });
-    } else if ($('#right_result').is(':checked')) {
-        $('input[name=add_result]').each((index, item) => {
-            r_result[index] = $(item).val()*1;
-        });
-    }
 
-    if(l_result.includes('') || l_result.length < 4) {
-        return $('#left_result').parent().click();
-    } else if(r_result.includes('') || r_result.length < 4) {
-        return $('#right_result').parent().click();
-    }   
+jQuery.fn.serializeObject = function() {
+    var obj = null;
+    try {
+        if (this[0].tagName && this[0].tagName.toUpperCase() == "FORM") {
+            var arr = this.serializeArray();
+            if (arr) {
+                obj = {};
+                jQuery.each(arr, function() {
+                    obj[this.name] = this.value;
+                });
+            }//if ( arr ) {
+        }
+    } catch (e) {
+        alert(e.message);
+    } finally {
+    }
+ 
+    return obj; 
+};
+
+
+$('#eventForm').submit(function() {
 
     $.ajax({
         method:'POST',                                           
         url: '/admin/actionEvent',
         data: {
-            event_code: $('#addEventCode').val()*1,
-            title: $('#addTitle').val(),
-            contents: $('#addContents').val(),
-            r_text: $('#add_l_text').val(),
-            l_text: $('#add_r_text').val(),
-            r_result: r_result,
-            l_result: l_result,
-            next_event: $("#add_next_event option:selected").val(),
+            formData: $(this).serializeObject(),
+            actionType: actionType,
             id: currentId,
             type: actionType
         },
@@ -95,16 +102,19 @@ $('#eventForm').submit(function() {
 });
 
 $('.addEvent').click(function() {
-    $('#addEventCode').val('');
-    $('#addTitle').val('');
-    $('#addContents').val('');
-    $('#add_l_text').val('');
-    $('#add_r_text').val('');
-    l_result = []; 
-    r_result = [];
-    $('input[name=add_result]').each((index, item) => {
+    $('#eventType').val('random').prop('selected', true);
+    $('#eventCode').val('');
+    $('#eventTitle').val('');
+    $('#eventContents').val('');
+    $('#l_text').val('');
+    $('#r_text').val('');
+    $('#left_result').click();
+    $('#left_event').click();
+    $('.stats > input').each((index, item) => {
         $(item).val('');
     });
+    $('#leftEvent').val('default').prop('selected', true);
+    $('#rightEvent').val('default').prop('selected', true);
 });
 let currentId;
 $('.editEvent').click(function(){
@@ -114,24 +124,29 @@ $('.editEvent').click(function(){
         url: "/admin/getEvent?id="+currentId,
         dataType: "json",
         success: function(result) {
-            $('#addEventCode').val(result.event_code);
-            $('#addTitle').val(result.title);
-            $('#addContents').val(result.contents);
-            $('#add_l_text').val(result.l_text);
-            $('#add_r_text').val(result.r_text);
-            l_result = result.l_result;
-            r_result = result.r_result;
-            if ($('#left_result').is(':checked')) {
-                $('input[name=add_result]').each((index, item) => {
-                    $(item).val(l_result[index]);
-                });
-            } else if ($('#right_result').is(':checked')) {
-                $('input[name=add_result]').each((index, item) => {
-                    $(item).val(r_result[index]);
-                });
+            $('#eventType').val(result.event_type).prop("select", true);
+            $('#eventCode').val(result.event_code*1);
+            $('#eventTitle').val(result.title);
+            $('#eventContents').val(result.contents);
+            $('#l_text').val(result.choices.left);
+            $('#r_text').val(result.choices.right);
+            
+            let rewards = [];
+            for (i in result.rewards) {
+                for(j in result.rewards[i]) {
+                    rewards.push(result.rewards[i][j]);
+                }
             }
-
-            $("#edit_next_event").val(result.next_event+'').prop("selected", true);
+            $('.stats > input').each((index, item) => {
+                $(item).val(rewards[index]);
+            });
+            
+            $('#leftEvent').val(
+                result.next_event.left ? result.next_event.left : 'default'
+            ).prop('selected', true);
+            $('#rightEvent').val(
+                result.next_event.right ? result.next_event.right : 'default'
+            ).prop('selected', true);
         },
         error: function(result, status, error) {
             console.log(error);
