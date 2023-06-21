c993f8bfd34569a89e893c7ee79e5b0e0f65c41a
Honey-Bi <biten10@naver.com>
2023-06-21 15:00:23 +0900

    안쓰는 코드 삭제

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,6 @@
 const router = require('express').Router();
 const { auth } = require('../middleware/userValidation');
-const userStatus = require('../middleware/userStatus')
-const jwt = require('jsonwebtoken');
-const config = require('../config/default.json');
 const Main = require('../models/Main');
-const SECRET_KEY = config.jwtSecretKey;
-
-// router.get('/', auth, async (req, res) => {
-//     const id = req.decoded.user.id;
-//     user = await User.findOne({ _id: id }) ;
-//     res.render('./main', {title: 'main', userName: user.name, isLogin: true});
-// });
-
 
 router.get('/', auth, (req, res) => {
     res.render('./main', {title: 'main', isLogin: true,});
