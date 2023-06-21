c7c1ae9c2cf7daaf6789070c333b837df085f36b
Honey-Bi <biten10@naver.com>
2023-06-21 11:16:52 +0900

    관리자용 미들웨어 생성 (구현 안함)

---
+++ middleware/adminValidation.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/default.json');
+const SECRET_KEY = config.jwtSecretKey;
+
+exports.admin = (req, res, next) => {
+    // 인증 완료
+    try {
+        // 요청 쿠키에 저장된 토큰과 비밀키를 사용하여 토큰을 req.decoded에 반환
+        req.decoded = jwt.verify(req.session.token, SECRET_KEY);
+        return next();
+    }
+    // 인증 실패
+    catch (error) {
+        // 유효시간이 초과된 경우
+        if (error.name === 'TokenExpiredError') {
+            return res.write("<script>alert('code: 419, message: 토큰이 만료되었습니다. 다시 로그인해 주세요'); location.href='/account/logout'</script>");
+            // return res.redirect('/account/logout');
+            // return res.status(419).json({
+                
+            // });
+        }
+        // 토큰의 비밀키가 일치하지 않는 경우
+        if (error.name === 'JsonWebTokenError') {
+            return res.redirect('/account/login');
+            // return res.status(401).json({
+            //     code: 401,
+            //     message: '유효하지 않은 토큰입니다.'
+            // });
+        }
+    }
+}
