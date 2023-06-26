abeef0bbea396ee17b3b00bd16a0782530d3d8e2
Honey-Bi <biten10@naver.com>
2023-06-26 15:10:11 +0900

    네이버 아이디 로그인 기능 추가

--- routes/account.js
+++ routes/account.js
@@ -6,23 +6,111 @@ const User = require('../models/User');
 const Main = require('../models/Main'); 
 const jwt = require('jsonwebtoken');
 const config = require('../config/default.json');
-const mongoose = require("mongoose")
-const SECRET_KEY = config.jwtSecretKey;
+const { default: mongoose } = require('mongoose');
 
-router.get('/login', (req, res) => {
-    res.render('./account/login', { title: 'Login', isLogin: userStatus.isLogin(req) });
+const SECRET_KEY = config.jwtSecretKey;
+const client_id = config.naver.client_id;
+const client_secret = config.naver.client_secret;
+const redirectURI = config.naver.redirectURI;
+
+router.get('/callback', function (req, res) {
+    code = req.query    .code;
+    state = req.query.state;
+    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+        + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+                // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                // res.end(body);
+                req.session.naver = JSON.parse(body);
+                res.redirect('/account/member');
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
+router.get('/member', function (req, res) {
+    var api_url = 'https://openapi.naver.com/v1/nid/me';
+    var request = require('request');
+    var token = req.session.naver.access_token;
+    var header = "Bearer " + token; 
+    var options = {
+        url: api_url,
+        headers: {
+            'Authorization': header,
+        }
+    };
+    request.get(options, async function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            // res.end(body);
+            const naver_res =JSON.parse(body).response;
+            
+            const user = await User.findOne({email: naver_res.email});
+            if (!user) { // 유저정보가 존재하지 않을시
+                const newUser = new User({
+                    name: naver_res.nickname,
+                    email: naver_res.email,
+                    password: ' ',
+                    idType: 'naver'
+                });
+                newUser.save();
+
+                console.log(newUser._id);
+                const event = await Main.MainEvent.findOne({ 
+                    event_type: 'link',
+                    event_code: 0 // 시작 이벤트
+                }, {_id:1})._id;
+
+                new Main.Main({
+                    userId: newUser._id,
+                    nowEvent: event,
+                }).save();
+                setToken(req, newUser._id);
+            } else {
+                setToken(req, user._id);
+            }
+            console.log('네이버 로그인성공');
+            res.redirect('/');
+        } else {
+            console.log('error');
+            if(response != null) {
+                res.status(response.statusCode).end();
+                console.log('error = ' + response.statusCode);
+            }
+            res.redirect('/account/login');
+        }
+    });
+});
+
+router.get('/login', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    state = await bcrypt.hash(Date.now().toString(), salt);
+    api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
+    res.render('./account/login', {
+        title: 'Login', 
+        isLogin: userStatus.isLogin(req),
+        api_url: api_url
+    });
 });
 
 router.post("/login-confirm", async (req, res) => {
     const id = req.body.id, 
           pw = req.body.pw;
     try {
-        let user = await User.findOne({email: id}).select('+password');
+        let user = await User.findOne({email: id, idType: 'basic'}).select('+password');
         if(!user){
             return res.status(401).json({
                 code: 401,
                 message: '아이디 혹은 비밀번호가 틀림니다.',
-            });;
+            });
         }
         console.log("id confirm");
         // await User.findOne({email: id}, (err, user)=>{
@@ -37,23 +125,8 @@ router.post("/login-confirm", async (req, res) => {
                     message: '아이디 혹은 비밀번호가 틀림니다.',
                 });;
             }
-            const payload = {
-                type: 'JWT',
-                user: {
-                    id: user._id,
-                },
-            };
-            token = jwt.sign(
-                payload,            // token으로 변환할 데이터
-                SECRET_KEY,        // secret key 값
-                { expiresIn: "1h", },// token의 유효시간을 1시간으로 설정
-            );
-            req.session.token = token;
-            // res.status(statusCode.OK).send(util.success(statusCode.OK, responseMsg.LOGIN_SUCCESS, {
-            //     /* 생성된 Token을 클라이언트에게 Response */
-            //      token: jwtToken.token                
-            // }));
 
+            setToken(req, user._id);
             return res.status(200).json({
                 code: 200,
                 message: '토큰이 발급되었습니다.',
@@ -94,7 +167,8 @@ router.post('/exist-confirm', async (req, res) => {
 router.post("/register-confirm", async (req, res) => {
     const name = req.body.name, 
           email = req.body.id, 
-          password = req.body.password;
+          password = req.body.password,
+          idType = req.body.idType;
     try {
         // email을 비교하여 user가 이미 존재하는지 확인
         // let user = await User.findOne({ email });
@@ -108,6 +182,7 @@ router.post("/register-confirm", async (req, res) => {
             name: name,
             email: email,
             password: password,
+            idType: (req.body.idType)?req.body.idType:'basic',
         });
   
         // password를 암호화 하기
@@ -127,6 +202,10 @@ router.post("/register-confirm", async (req, res) => {
             nowEvent: event,
         }).save();
 
+        if (req.body.idType != 'test') {
+            setToken(req, user._id);
+        }
+
         res.send(true);
     } catch (error) {
         console.error(error.message);
@@ -137,12 +216,12 @@ router.post("/register-confirm", async (req, res) => {
 router.get('/logout', (req, res) => {
     var session = req.session;
     try {
-        if (session.token) { //세션정보가 존재하는 경우
+        if (session.token || session.naver) { //세션정보가 존재하는 경우
             req.session.destroy(function (err) {
                 if (err)
                     console.log(err);
                 else {
-                    res.redirect('/');
+                    res.redirect(req.query.callback);
                 }
             })
         }
@@ -162,4 +241,20 @@ router.get('/pw-forgot', (req, res) => {
         isLogin: false
     })
 });
+
 module.exports = router;
+
+function setToken(req, id) {
+    const payload = {
+        type: 'JWT',
+        user: {
+            id: id,
+        },
+    };
+    token = jwt.sign(
+        payload,                // token으로 변환할 데이터
+        SECRET_KEY,             // secret key 값
+        { expiresIn: "1h", },// token의 유효시간을 1시간으로 설정
+    );
+    req.session.token = token;
+}
