75d94a03381124bf27858a229e41699d3e62de49
Honey-Bi <biten10@naver.com>
2023-07-05 13:53:39 +0900

    9

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -13,7 +13,8 @@ exports.auth = (req, res, next) => {
     catch (error) {
         // 유효시간이 초과된 경우
         if (error.name === 'TokenExpiredError') {
-            res.write("<script>alert('code: 419, message: 토큰이 만료되었습니다. 다시 로그인해 주세요'); location.href='/account/logout'</script>");
+            res.write("<script>alert('code: 419, message: login time over. retry sign in'); location.href='/account/logout'</script>");
+            res.write("<script>window.location=\"/account/logout\"</script>");
             return res.redirect('/account/logout');
             // return res.status(419).json({
                 
